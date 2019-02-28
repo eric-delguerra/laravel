100b6b6cdd9bcd0401a55c7b4e1a81a39d23af6b
Eric Del Guerra <eric.del-guerra@Digital-Grenoble.local>
2019-02-28 17:10:06 +0100

    Modif layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,9 @@
-
-
-
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>@yield('title')</title>
-
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
@@ -21,8 +16,6 @@
     <a href="/basket">Panier</a>
     <a href="/contacts">Contact</a>
 </div>
-
-
 <div class="content">
     <div class="title m-b-md">
         <div class="links">
@@ -33,11 +26,5 @@
     </div>
  @yield('petitforeach')
 </div>
-
-
-
-
-
-
 </body>
 </html>
