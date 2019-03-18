8362581d07b5f6e29c6f2efa77dbc03719c0478a
Eric Del Guerra <eric.del-guerra@Digital-Grenoble.local>
2019-03-18 11:31:14 +0100

    Add all Migrations

---
+++ database/migrations/2019_03_18_101929_products.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('img');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('weigth');
+            $table->unsignedInteger('stock');
+            $table->unsignedInteger('categories_id');
+            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_03_18_102938_categories.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Categories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
