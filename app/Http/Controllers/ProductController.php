e42c397e6cfb3bf9cbba6cf5d55041b3bf62114d
Cedric-Alonso <cedric.g.alonso@gmail.com>
2019-03-22 16:43:35 +0100

    ajout des prix apres reduction // promo terminer

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,8 @@ class productController extends Controller
     {
         if (isset($_GET['price'])) {
             $products = Product::all()->sortBy('price');
-            return view('products.products', ['products' => $products]);
+            $reduc = $this->reduc($products);
+            return view('products.products', ['products' => $products], ['reduc' =>$reduc]);
         }
         if (isset($_GET['stock'])) {
             $products = Product::all()->sortBy('stock');
@@ -31,4 +32,35 @@ class productController extends Controller
         $results = Product::find($id);
         return view('products/product', ['product' => $results]);
     }
+
+    private function reduc($products){
+        $reducs = array();
+        $promos = Promo::all();
+        foreach ($products as $key=>$product){
+            foreach ($promos as $key=>$promo) {
+                if ($product->promo['id'] == $promo->id) {
+                    switch ($product->promo->type){
+                        case 1 :
+                            $reduc = $product->price - $product->promo->value;
+                            if ($reduc<0){
+                                $reduc = 0;
+                            }
+                            $reducs[$product->id] =  $reduc;
+                        break;
+                        case 2:
+                            $reduc = $product->price - (($product->price * $product->promo->value) /100);
+                            if ($reduc<0){
+                                $reduc = 0;
+                            }
+                            $reducs[$product->id] =  $reduc;
+                        break;
+                        default:
+                            $reducs[$product->id] =  null;
+                        break;
+                    }
+                }
+            }
+        }
+        return $reducs;
+     }
 }
